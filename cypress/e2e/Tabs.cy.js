1042974e987c0ff6c634e17d72304a70afaff5b7
AshimMaharjan27 <ashim.maharjan27@gmail.com>
2022-12-20 19:49:16 -0600

    new update

--- cypress/e2e/Tabs.cy.js
+++ cypress/e2e/Tabs.cy.js
@@ -10,19 +10,19 @@ describe('Handle Tap ', () => {
         cy.go('back')
     }
     )
-    // it('Approch2', () => {
+    it('Approch2', () => {
 
-    //     cy.visit("https://the-internet.herokuapp.com/windows")
-    //     cy.wait(3000)
-    //     cy.get('div.example>a').then((e)=>{
-    //         let url = e.prop('href')
-    //         cy.visit(url)
-    //     })
-    //     cy.url().should('eq', 'https://the-internet.herokuapp.com/windows/new')
-    //     cy.wait(3000)
-    //     cy.go('back')
-    // }
-    // )
+        cy.visit("https://the-internet.herokuapp.com/windows")
+        cy.wait(3000)
+        cy.get('div.example>a').then((e)=>{
+            let url = e.prop('href')
+            cy.visit(url)
+        })
+        cy.url().should('eq', 'https://the-internet.herokuapp.com/windows/new')
+        cy.wait(3000)
+        cy.go('back')
+    }
+    )
 
 
 
